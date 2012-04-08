ca2b62520ac3b24c97b65e31d0e4f8fe836262a0
GrzegorzKozub <grzegorz.kozub@gmail.com>
2012-04-08 22:53:39 +0200

    finished working on the OpenSSL abstraction

--- Generate-Certificate.ps1
+++ Generate-Certificate.ps1
@@ -2,9 +2,27 @@
     [CmdletBinding()]
 
     param (
+        [Parameter()]
+        [switch]
+        $DontGenerateRootCa,
 
+        [Parameter()]
+        [switch]
+        $UseSubCa,
+
+        [Parameter()]
+        [switch]
+        $DontGenerateSubCa,
+
+        [Parameter()]
+        [switch]
+        $RevokeSubCa,
+
+        [Parameter()]
+        [switch]
+        $RevokeCertificate
     )
-    
+
     # check for OpenSSL availability
     
     $openssl = Get-Command openssl -ErrorAction SilentlyContinue
@@ -43,6 +61,8 @@
             <config>
                 <rootCa>
                     <filename>RootCA</filename>
+                    <validForDays>365</validForDays>
+                    <keyPassword>pass</keyPassword>
                     <subject>
                         <c>PL</c>
                         <st>mazowieckie</st>
@@ -55,6 +75,8 @@
                 </rootCa>
                 <subCa>
                     <filename>SubCA</filename>
+                    <validForDays>365</validForDays>
+                    <keyPassword>pass</keyPassword>
                     <subject>
                         <c>PL</c>
                         <st>mazowieckie</st>
@@ -67,6 +89,9 @@
                 </subCa>
                 <certificate>
                     <filename>Certificate</filename>
+                    <validForDays>365</validForDays>
+                    <keyPassword>pass</keyPassword>
+                    <exportPassword>pass</exportPassword>
                     <subject>
                         <c>PL</c>
                         <st>mazowieckie</st>
@@ -82,7 +107,7 @@
         $config.Save($configFile)
         
         Write-Host "The $configFile file was created. Modify it now, if required, then press any key to continue..."
-        $null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
+        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyUp")
     }
     
     # read configuration
@@ -92,46 +117,118 @@
     $config = New-Object xml
     $config.Load($configFile)
     
-    $rootCaFilename = $config.config.rootCa.filename
-    $subCaFilename = $config.config.subCa.filename
-    $certificateFilename = $config.config.certificate.filename
-    
     $subjectPattern = "/countryName={0}/stateOrProvinceName={1}/localityName={2}/organizationName={3}/organizationalUnitName={4}/commonName={5}/emailAddress={6}/"
-    
+
+    $rootCaFilename = $config.config.rootCa.filename
+    $rootCaValidForDays = $config.config.rootCa.validForDays
+    $rootCaKeyPassword = $config.config.rootCa.keyPassword
     $rootCaSubject = $config.config.rootCa.subject
-    $rootCaSubject = [string]::Format($subjectPattern, $rootCaSubject.c, $rootCaSubject.st, $crootCaSubject.l, $rootCaSubject.o, $rootCaSubject.ou, $rootCaSubject.cn, $rootCaSubject.e)
-    
-    $subCaSubject = $config.config.subCa.subject
-    $subCaSubject = [string]::Format($subjectPattern, $subCaSubject.c, $subCaSubject.st, $csubCaSubject.l, $subCaSubject.o, $subCaSubject.ou, $subCaSubject.cn, $subCaSubject.e)
-   
+    $rootCaSubject = [string]::Format($subjectPattern, $rootCaSubject.c, $rootCaSubject.st, $rootCaSubject.l, $rootCaSubject.o, $rootCaSubject.ou, $rootCaSubject.cn, $rootCaSubject.e)    
+
+    if ($UseSubCa) {
+        $subCaFilename = $config.config.subCa.filename
+        $subCaValidForDays = $config.config.subCa.validForDays
+        $subCaKeyPassword = $config.config.subCa.keyPassword
+        $subCaSubject = $config.config.subCa.subject
+        $subCaSubject = [string]::Format($subjectPattern, $subCaSubject.c, $subCaSubject.st, $subCaSubject.l, $subCaSubject.o, $subCaSubject.ou, $subCaSubject.cn, $subCaSubject.e)
+    }
+
+    $certificateFilename = $config.config.certificate.filename
+    $certificateValidForDays = $config.config.certificate.validForDays
+    $certificateKeyPassword = $config.config.certificate.keyPassword
+    $certificateExportPassword = $config.config.certificate.exportPassword
     $certificateSubject = $config.config.certificate.subject
-    $certificateSubject = [string]::Format($subjectPattern, $certificateSubject.c, $certificateSubject.st, $ccertificateSubject.l, $certificateSubject.o, $certificateSubject.ou, $certificateSubject.cn, $certificateSubject.e)
-
-    # generate the root CA certificate and key
-    # Write-Verbose
-
-    return
+    $certificateSubject = [string]::Format($subjectPattern, $certificateSubject.c, $certificateSubject.st, $certificateSubject.l, $certificateSubject.o, $certificateSubject.ou, $certificateSubject.cn, $certificateSubject.e)
     
-    # generate the root CA certificate and key
-    openssl req -new -x509 -days 3650 -out RootCA.cer -keyout RootCA.key -subj /countryName="PL"/stateOrProvinceName="mazowieckie"/localityName="Warsaw"/organizationName="Hewlett-Packard"/organizationalUnitName="BS ITO Software Services"/commonName="Test Root CA"/emailAddress="grzegorz.kozub@hp.com"/
-    # generate the subordinate CA certificate request and key
-    openssl req -new -out SubCA.req -keyout SubCA.key -subj /countryName="PL"/stateOrProvinceName="mazowieckie"/localityName="Warsaw"/organizationName="Hewlett-Packard"/organizationalUnitName="BS ITO Software Services"/commonName="Test Sub CA"/emailAddress="grzegorz.kozub@hp.com"/
-    # sign the subordinate CA with the root CA key
-    openssl ca -days 3650 -extensions v3_ca -cert RootCA.cer -keyfile RootCA.key -in SubCA.req -out SubCA.cer
-    # generate the certificate request
-    openssl req -new -out Certificate.req -keyout Certificate.key -subj /countryName="PL"/stateOrProvinceName="mazowieckie"/localityName="Warsaw"/organizationName="Hewlett-Packard"/organizationalUnitName="BS ITO Software Services"/commonName="Test certificate"/emailAddress="grzegorz.kozub@hp.com"/
-    # sign the certificate request with the subordinate CA key
-    openssl ca -days 3650 -cert SubCA.cer -keyfile SubCA.key -in Certificate.req -out Certificate.cer
-    # convert the certificate to PKCS#12 format
-    openssl pkcs12 -export -inkey Certificate.key -in Certificate.cer -out Certificate.pfx
-    # revoke the certificate
-    openssl ca -cert SubCA.cer -keyfile SubCA.key -revoke Certificate.cer
-    # enerate the CRL for the subordinate CA
-    openssl ca -cert SubCA.cer -keyfile SubCA.key -gencrl -out SubCA.crl
-    # revoke the subordinate CA
-    openssl ca -cert RootCA.cer -keyfile RootCA.key -revoke SubCA.cer
-    # generate the CRL for the root CA
-    openssl ca -cert RootCA.cer -keyfile RootCA.key -gencrl -out RootCA.crl   
+    # setup file extensions
+
+    $cer = ".cer"
+    $key = ".key"
+    $pfx = ".crl"
+    $req = ".req"
+
+    # validate configuration
+
+    if (!$rootCaFilename -or !$rootCaValidForDays -or !$rootCaKeyPassword -or !$rootCaSubject) {
+        Write-Error "The $configFile file is missing required root CA settings."
+        return
+    }
+
+    if ($DontGenerateRootCa -and !($(Test-Path $rootCaFilename$cer) -and $(Test-Path $rootCaFilename$key))) {
+        Write-Error "If DontGenerateRootCa is set, the $rootCaFilename$cer and $rootCaFilename$key files must exist."
+        return
+    }
+
+    if ($UseSubCa) {
+        if (!$subCaFilename -or !$subCaValidForDays -or !$subCaKeyPassword -or !$subCaSubject) {
+            Write-Error "The $configFile file is missing required subordinate CA settings."
+            return
+        }
+
+        if ($DontGenerateSubCa -and !($(Test-Path $subCaFilename$cer) -and $(Test-Path $subCaFilename$key))) {
+            Write-Error "If DontGenerateSubCa is set, the $subCaFilename$cer and $subCaFilename$key files must exist."
+            return
+        }        
+    }
+
+    if (!$certificateFilename -or !$certificateValidForDays -or !$certificateKeyPassword -or !$certificateExportPassword -or !$certificateSubject) {
+        Write-Error "The $configFile file is missing required certificate settings."
+        return
+    }
+
+    # generate the CA
+
+    if (!$DontGenerateRootCa) {
+        Write-Verbose "Generating the root CA certificate and key."
+        openssl req -new -x509 -days $rootCaValidForDays -out $rootCaFilename$cer -keyout $rootCaFilename$key -subj $rootCaSubject -passout pass:$rootCaKeyPassword
+    }
+
+    if ($UseSubCa) {
+        if (!$DontGenerateSubCa) {
+            Write-Verbose "Generating the subordinate CA certificate request and key."
+            openssl req -new -out $subCaFilename$req -keyout $subCaFilename$key -subj $subCaSubject -passout pass:$subCaKeyPassword
+    
+            Write-Verbose "Signing the subordinate CA with the root CA key."
+            openssl ca -days $subCaValidForDays -extensions v3_ca -batch -cert $rootCaFilename$cer -keyfile $rootCaFilename$key -passin pass:$rootCaKeyPassword -in $subCaFilename$req -out $subCaFilename$cer
+        }
+
+        $caFilename = $subCaFilename
+        $caKeyPassword = $subCaKeyPassword
+        $caAdjective = "subordinate"
+    } else {
+        $caFilename = $rootCaFilename
+        $caKeyPassword = $rootCaKeyPassword
+        $caAdjective = "root"
+    }
+
+    # generate the certificate
+
+    Write-Verbose "Generating the certificate request."
+    openssl req -new -out $certificateFilename$req -keyout $certificateFilename$key -subj $certificateSubject -passout pass:$certificateKeyPassword
+
+    Write-Verbose "Signing the certificate request with the $caAdjective CA key."
+    openssl ca -days $certificateValidForDays -batch -cert $caFilename$cer -keyfile $caFilename$key -passin pass:$caKeyPassword -in $certificateFilename$req -out $certificateFilename$cer
+
+    Write-Verbose "Converting the certificate to PKCS#12 format."
+    openssl pkcs12 -export -in $certificateFilename$cer -inkey $certificateFilename$key -passin pass:$certificateKeyPassword -out $certificateFilename$pfx -passout pass:$certificateExportPassword
+
+    # generate the CRL
+
+    if ($UseSubCa -and $RevokeSubCa) {
+        Write-Verbose "Revoking the subordinate CA."
+        openssl ca -cert $rootCaFilename$cer -keyfile $rootCaFilename$key -passin pass:$rootCaKeyPassword -revoke $subCaFilename$cer
+
+        Write-Verbose "Generating the CRL for the root CA."
+        openssl ca -cert $rootCaFilename$cer -keyfile $rootCaFilename$key -passin pass:$rootCaKeyPassword -gencrl -out $rootCaFilename$crl   
+    }
+
+    if ($RevokeCertificate) {
+        Write-Verbose "Revoking the certificate."
+        openssl ca -cert $caFilename$cer -keyfile $caFilename$key -passin pass:$caKeyPassword -revoke $certificateFilename$cer
+
+        Write-Verbose "Generating the CRL for the $caAdjective CA."
+        openssl ca -cert $caFilename$cer -keyfile $caFilename$key -passin pass:$caKeyPassword -gencrl -out $caFilename$crl
+    }
 }
 
 Set-Alias generate Generate-Certificate
