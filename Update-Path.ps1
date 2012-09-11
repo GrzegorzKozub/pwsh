65c3079801d70d6b43e6e337a4a7fb49c729095f
GrzegorzKozub <grzegorz.kozub@gmail.com>
2012-09-11 21:04:12 +0200

    gave Update-Path.ps1 ability to smart sort the paths

--- Update-Path.ps1
+++ Update-Path.ps1
@@ -19,13 +19,14 @@
     )
 
     $path = [Environment]::GetEnvironmentVariable("Path", $Target)
-    $pathArray = $path.Split(";") 
+    $pathArray = $path.Split(";")
 
     if (!$Value) {
         $pathArray
         return
     }
 
+    $Value = $Value.TrimEnd("\")
     $valueExists = $pathArray | Where-Object { $_ -eq $Value }
 
     if ($Delete) {
@@ -33,8 +34,8 @@
             Write-Error "The value $Value does not exist in the $Target scope Path environment variable."
             return
         }
-        
-        $path = $( $pathArray | Where-Object { $_ -ne $Value } ) -Join ";"
+
+        $pathArray = $pathArray | Where-Object { $_ -ne $Value }
 
     } else {
         if ($valueExists) {
@@ -42,8 +43,25 @@
             return
         }
 
-        $path = $path + ";" + $Value
+        $pathArray += $Value
     }
+
+    $windows = Get-Content Env:SystemRoot
+    $programFiles = Get-Content Env:ProgramFiles
+    $programFilesx86 = Get-Content Env:"ProgramFiles(x86)"
+    $common = "C:\Common"
+    $programs = "C:\Programs"
+    $user = Get-Content Env:Home
+
+    $windowsPaths = $pathArray | Where-Object { $_ -like "*$windows\*" -or $_ -eq $windows }
+    $programFilesPaths = $pathArray | Where-Object { $_ -like "*$programFiles\*" } | Sort-Object
+    $programFilesx86Paths = $pathArray | Where-Object { $_ -like "*$programFilesx86\*" } | Sort-Object
+    $commonPaths = $pathArray | Where-Object { $_ -eq $common }
+    $programsPaths = $pathArray | Where-Object { $_ -like "*$programs\*" } | Sort-Object
+    $userPaths = $pathArray | Where-Object { $_ -like "*$user\*" } | Sort-Object
+
+    $pathArray = @() + $windowsPaths + $programFilesPaths + $programFilesx86Paths + $commonPaths + $programsPaths + $userPaths
+    $path = $( $pathArray | Where-Object { $_ -ne $null } ) -Join ";"
 
     [Environment]::SetEnvironmentVariable("Path", $path, $Target)
 }
