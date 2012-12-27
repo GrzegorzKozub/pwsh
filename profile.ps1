bb1ad8abd3e786b9d7925e0fc3297ccc76a7899d
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2012-12-27 09:16:10 +0100

    automated the way profile.ps1 sources the scripts

--- profile.ps1
+++ profile.ps1
@@ -1,24 +1,19 @@
 $MaximumHistoryCount = 32767
 
 $historyFile = Join-Path $HOME "WindowsPowerShellHistory.xml"
-Register-EngineEvent PowerShell.Exiting { Get-History -Count $MaximumHistoryCount | Group-Object CommandLine | ForEach-Object { $_.Group[0] } | Export-Clixml $historyFile } -SupportEvent
+Register-EngineEvent PowerShell.Exiting { 
+    Get-History -Count $MaximumHistoryCount |
+    Group-Object CommandLine |
+    ForEach-Object { $_.Group[0] } |
+    Export-Clixml $historyFile 
+} -SupportEvent
 
 if (Test-Path $historyFile) {
     Import-CliXml $historyFile | Add-History 
 }
 
-Push-Location
-cd $(Split-Path $PROFILE)
-
-. .\Attach-Image.ps1
-. .\Encrypt-Config.ps1
-. .\Generate-Certificate.ps1
-. .\Import-Certificate.ps1
-. .\Process-ResourceHackerScripts.ps1
-. .\Set-Proxy.ps1
-. .\Set-VisualStudioVars.ps1
-. .\Update-Path.ps1
-
-Pop-Location
+Get-ChildItem $(Join-Path $(Split-Path $PROFILE) *.ps1) `
+    -Exclude $(Split-Path $PROFILE.CurrentUserAllHosts -Leaf), $(Split-Path $PROFILE.CurrentUserCurrentHost -Leaf) |
+    ForEach-Object { . $_ }
 
 Import-Module posh-git
