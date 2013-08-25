27d11998c7885bd669f949a12c7feeb573a55689
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2013-08-25 19:31:09 +0200

    Change Ruby version to 2.0.0 in Build-Vim.ps1.

--- Build-Vim.ps1
+++ Build-Vim.ps1
@@ -18,7 +18,7 @@ function Build-Vim {
             FEATURES=HUGE GUI=$gui OLE=$gui `
             PYTHON3=c:/Programs/Python DYNAMIC_PYTHON3=yes PYTHON3_VER=33 `
             PERL=c:/Programs/Perl DYNAMIC_PERL=yes PERL_VER=514 `
-            RUBY=c:/Programs/Ruby DYNAMIC_RUBY=yes RUBY_VER=19 RUBY_VER_LONG=1.9.1
+            RUBY=c:/Programs/Ruby DYNAMIC_RUBY=yes RUBY_VER=20 RUBY_VER_LONG=2.0.0
     }
 
     Copy-Item -Path "vim.exe", "gvim.exe" -Destination $(Get-Location -Stack).ToArray()[0].Path -Force
