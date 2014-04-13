ba21201ac6c2eafb25737608eba02d7085b9c6b5
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2014-04-13 19:53:07 +0200

    Change Python version used for Vim from 3.3 to 2.7.

--- Build-Vim.ps1
+++ Build-Vim.ps1
@@ -16,7 +16,7 @@ function Build-Vim {
     foreach ($gui in "no", "yes") {
         mingw32-make -j4 -f make_ming.mak `
             FEATURES=HUGE GUI=$gui OLE=$gui `
-            PYTHON3=c:/Programs/Python DYNAMIC_PYTHON3=yes PYTHON3_VER=33 `
+            PYTHON=c:/Programs/Python DYNAMIC_PYTHON=yes PYTHON_VER=27 `
             PERL=c:/Programs/Perl DYNAMIC_PERL=yes PERL_VER=514 `
             RUBY=c:/Programs/Ruby DYNAMIC_RUBY=yes RUBY_VER=20 RUBY_VER_LONG=2.0.0
     }
