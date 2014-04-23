35c1d430b0b635a7f5f577c9a8425dc726da0515
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2014-04-23 08:45:12 +0200

    Add a script for caching PowerShell assemblies in GAC.

---
+++ Update-Gac.ps1
@@ -0,0 +1,11 @@
+Set-Alias ngen (Join-Path ([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe)
+
+[AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.Location -ne $null } | ForEach-Object {
+    $assemblyName = Split-Path $_.Location -Leaf
+    if ([System.Runtime.InteropServices.RuntimeEnvironment]::FromGlobalAccessCache($_)) {
+        Write-Host "Already in Native Images Cache: $assemblyName."
+    } else {
+        Write-Host "Generating native images for: $assemblyName."
+        ngen $_.Location | ForEach-Object { "`t$_" }
+    }
+}
