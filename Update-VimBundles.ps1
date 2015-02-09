24bb71dffc6156dabebd209958aed5867a2c7e03
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2015-02-09 20:09:16 +0100

    Support git submodules and npm packages.

--- Update-VimBundles.ps1
+++ Update-VimBundles.ps1
@@ -1,16 +1,32 @@
 function Update-VimBundles {
-
     workflow Pull-UpdatesInParallel {
-        $bundles = Get-ChildItem -Path "c:\Programs\Vim\vimfiles\bundle" -Directory -Exclude "YouCompleteMe"
+        $bundles = Get-ChildItem -Path "c:\Programs\Vim\vimfiles\bundle" -Directory
 
         foreach -Parallel ($bundle in $bundles) {
-
             InlineScript {
                 $bundle = $Using:bundle
-                Set-Location -Path $bundle.FullName
-                $result = git pull 2>&1
                 $n = [Environment]::NewLine
-                Write-Output -InputObject $("Updating $bundle..." + $n + [String]::Join($n, $result) + $n)
+
+                Set-Location -Path $bundle.FullName
+
+                $pullResult = git pull 2>&1
+                Write-Output -InputObject $("Pulling $bundle..." + $n + [String]::Join($n, $pullResult) + $n)
+
+                if (git submodule 2>&1) {
+                    $submoduleUpdateResult = git submodule update --init --recursive 2>&1
+
+                    if ($submoduleUpdateResult) {
+                        Write-Output -InputObject $("Updating submodules for $bundle..." + $n + [String]::Join($n, $submoduleUpdateResult) + $n)
+                    }
+                }
+
+                if (Test-Path "package.json" -PathType Leaf) {
+                    $npmInstallResult = npm install 2>&1
+
+                    if ($npmInstallResult) {
+                        Write-Output -InputObject $("Installing npm packages for $bundle..." + $n + [String]::Join($n, $npmInstallResult) + $n)
+                    }
+                }
             }
         }
     }
