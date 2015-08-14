163e0c89bb0cfd98604d0cc17fb4f936362018c9
Grzegorz Kozub <grzegorz.kozub@apsis.com>
2015-08-14 21:37:32 +0200

    Remove custom command history persistence

--- profile.ps1
+++ profile.ps1
@@ -1,17 +1,3 @@
-$MaximumHistoryCount = 32767
-
-$historyFile = Join-Path $HOME "WindowsPowerShellHistory.xml"
-Register-EngineEvent PowerShell.Exiting { 
-    Get-History -Count $MaximumHistoryCount |
-    Group-Object CommandLine |
-    ForEach-Object { $_.Group[0] } |
-    Export-Clixml $historyFile 
-} -SupportEvent
-
-if (Test-Path $historyFile) {
-    Import-CliXml $historyFile | Add-History 
-}
-
 Get-ChildItem $(Join-Path $(Split-Path $PROFILE) *.ps1) `
     -Exclude $(Split-Path $PROFILE.CurrentUserAllHosts -Leaf), $(Split-Path $PROFILE.CurrentUserCurrentHost -Leaf) |
     ForEach-Object { . $_ }
