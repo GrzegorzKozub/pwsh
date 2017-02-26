742c0d3a42d42d1d2bac40c1cbeb1c96611d2f7d
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2017-02-26 20:12:22 +0100

    Remove unnecessary workaround

--- Edit-Hosts.ps1
+++ Edit-Hosts.ps1
@@ -1,5 +1,5 @@
 function Edit-Hosts {
-    Start-Process -FilePath "gvim.exe" -ArgumentList "--cmd ""let g:signify_disable_by_default = 1""", "C:\Windows\System32\drivers\etc\hosts" -Verb RunAs
+    Start-Process -FilePath "gvim.exe" -ArgumentList "C:\Windows\System32\drivers\etc\hosts" -Verb RunAs
 }
 
 Set-Alias hosts Edit-Hosts
