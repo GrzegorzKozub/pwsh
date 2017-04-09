cdc2fae385b0998b331a7c0e651b6f6f18d5b5d0
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2017-04-09 22:31:35 +0200

    Provide binaries, runtime and custom files by Build-Vim

--- Build-Vim.ps1
+++ Build-Vim.ps1
@@ -1,46 +1,87 @@
-function Build-Vim {
+﻿function Build-Vim {
     [CmdletBinding()]
 
     param (
         [Parameter(Position = 0, Mandatory = $true)]
         [ValidateScript({ Test-Path $_ })]
         [string]
-        $Path
+        $WorkDir,
+
+        [Parameter(Position = 1, Mandatory = $true)]
+        [ValidateScript({ Test-Path $_ })]
+        [string]
+        $OutDir
     )
 
     Push-Location
-    Set-Location $Path
-    $cloneDir = "Build-Vim"
 
-    if (Test-Path $cloneDir -PathType Container) {
-        Set-Location $cloneDir
+    $zipFile = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($PROFILE.CurrentUserAllHosts), "Build-Vim.zip")
+
+    $WorkDir = Resolve-Path $WorkDir
+    $OutDir = Resolve-Path $OutDir
+
+    Set-Location $WorkDir
+
+    $vim = "vim"
+
+    if (Test-Path $vim -PathType Container) {
+        Set-Location $vim
         git clean -fd
         git pull
     } else {
-        git clone https://github.com/vim/vim.git $cloneDir
-        Set-Location $cloneDir
+        git clone https://github.com/vim/vim.git $vim
+        Set-Location $vim
     }
 
-    $archivePath = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($PROFILE.CurrentUserAllHosts), "Build-Vim.zip")
-    7z x -y $archivePath *.diff | Out-Null
+    7z x -y $zipFile *.diff | Out-Null
 
     foreach ($patch in Get-ChildItem -Filter *.diff) {
         git apply $patch
     }
 
     Set-Location "src"
-    7z x -y $archivePath | Out-Null
+
+    7z x -y $zipFile *.ico | Out-Null
 
     foreach ($gui in "no", "yes") {
         mingw32-make -j2 -f make_ming.mak `
-            FEATURES=HUGE MBYTE=yes IME=yes GIME=yes CSCOPE=yes GUI=$gui OLE=$gui DIRECTX=$gui `
+            FEATURES=HUGE MBYTE=yes IME=yes GIME=yes CSCOPE=yes NETBEANS=no GUI=$gui OLE=$gui DIRECTX=$gui `
             PYTHON3=d:/Apps/Python DYNAMIC_PYTHON3=yes PYTHON3_VER=35 `
             PERL=d:/Apps/Perl DYNAMIC_PERL=yes PERL_VER=524 `
-            RUBY=d:/Apps/Ruby DYNAMIC_RUBY=yes RUBY_VER=23 RUBY_VER_LONG=2.3.0 `
-            #LUA=d:/Apps/Lua DYNAMIC_LUA=yes LUA_VER=52
+            RUBY=d:/Apps/Ruby DYNAMIC_RUBY=yes RUBY_VER=23 RUBY_VER_LONG=2.3.0
     }
 
-    Copy-Item -Path "vim.exe", "gvim.exe", "vimrun.exe" -Destination $Path -Force
+    $runtime = (Select-String -Path "version.h" -Pattern '#define VIM_VERSION_NODOT.\"(vim[0-9]{2})\"').Matches[0].Groups[1].Value
+
+    Set-Location "tee"
+    mingw32-make
+
+    Set-Location $WorkDir
+
+    $runtimeDir = Join-Path $OutDir $runtime
+    $wimfilesDir = Join-Path $OutDir "vimfiles"
+
+    Remove-Item -Path $runtimeDir -Recurse -ErrorAction SilentlyContinue
+    Remove-Item -Path $wimfilesDir -Recurse -ErrorAction SilentlyContinue
+
+    Copy-Item `
+        -Path "$vim\runtime" `
+        -Destination $runtimeDir `
+        -Recurse `
+        -Exclude *.cdr, *.desktop, *.eps, *.gif, *.info, *.pdf, *.png, *.xpm, termcap
+
+    Copy-Item `
+        -Path "$vim\src\vim.exe", "$vim\src\gvim.exe", "$vim\src\vimrun.exe", "$vim\src\tee\tee.exe", "$vim\src\xxd\xxd.exe" `
+        -Destination $runtimeDir
+
+    7z x -y $zipFile *.bat *.dll *.exe -o"$runtimeDir" | Out-Null
+
+    $autoloadPath = New-Item -Path (Join-Path $wimfilesDir "autoload") -ItemType Directory
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile (Join-Path $autoloadPath "plug.vim")
+
+    $spellPath = New-Item -Path (Join-Path $wimfilesDir "spell") -ItemType Directory
+    7z x -y $zipFile *.spl -o"$spellPath" | Out-Null
+
     Pop-Location
 }
 
