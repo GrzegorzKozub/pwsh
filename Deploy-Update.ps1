340cc6712be6c98fbae87f59f57dfc698fa520a4
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2018-12-22 10:47:11 +0100

    Support updating all installed packages

---
+++ Deploy-Update.ps1
@@ -0,0 +1,34 @@
+﻿function Deploy-Update {
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Position = 0)]
+        [switch]
+        $Parallel,
+
+        [Parameter(Position = 1)]
+        [string]
+        $Source,
+
+        [Parameter(Position = 2)]
+        [string]
+        $Target
+    )
+
+    if (!(Test-Admin)) {
+        Write-Error "Must run as admin"
+        return
+    }
+
+    $time = [Diagnostics.Stopwatch]::StartNew()
+
+    foreach ($app in $(Get-ChildItem -Path $(Join-Path $(if ($Target) { $Target } else { "D:" }) "Apps") -Directory -Name)) {
+        Deploy-App -App $app -Update -Parallel: $Parallel -Source: $Source -Target: $Target
+    }
+
+    $time.Stop()
+    Write-Host "All done in $($time.Elapsed.ToString("mm\:ss\.fff"))" -ForegroundColor DarkGray
+}
+
+Set-Alias update Deploy-Update
+
