ba72146bb730ea0f703438b19e896c7292ac031e
greg <grzegorz.kozub@gmail.com>
2019-12-03 12:31:56 +0100

    Automate getting Windows build from an ISO image

---
+++ Get-WindowsIsoVersion.ps1
@@ -0,0 +1,20 @@
+function Get-WindowsIsoVersion {
+  [CmdletBinding()]
+
+  param (
+    [Parameter(Position = 0, Mandatory = $true)]
+    [ValidateScript({ Test-Path $_ })]
+    [string]
+    $IsoPath
+  )
+
+  if (!(Test-Admin)) {
+      Write-Error "Must run as admin"
+      return
+  }
+
+  Mount-DiskImage -ImagePath $IsoPath | Out-Null
+  (dism.exe /Get-WimInfo /WimFile:"e:\sources\install.esd" /Index:1 | Select-String -Pattern "Version : ").ToString().Split(".")[2]
+  Dismount-DiskImage -ImagePath $IsoPath | Out-Null
+}
+
