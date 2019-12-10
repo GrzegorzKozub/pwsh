c31602ff0ea99429de26ecfcaa314fee33d4d3c3
greg <grzegorz.kozub@gmail.com>
2019-12-10 15:42:57 +0100

    Create script to backup GitHub public repos

---
+++ Backup-Repos.ps1
@@ -0,0 +1,98 @@
+function Backup-Repos {
+  [CmdletBinding()]
+
+  param (
+    [Parameter(Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $GitHubUser = "GrzegorzKozub",
+
+    [Parameter(Position = 1)]
+    [ValidateScript({ Test-Path $_ })]
+    [string]
+    $To = "D:\Dropbox\GitHub"
+  )
+
+  function GetRepos {
+    return (Invoke-WebRequest -Uri "https://api.github.com/users/$GitHubUser/repos" |
+      ConvertFrom-Json) |
+      Select-Object -Property `
+        @{ Name = 'name'; Expression = { $_.name } }, `
+        @{ Name = 'url'; Expression = { $_.ssh_url } }
+  }
+
+  function GetRepoVersion ($repoName) {
+    return (Invoke-WebRequest -Uri "https://api.github.com/repos/$GitHubUser/$repoName/branches/master" |
+      ConvertFrom-Json).commit.sha.Substring(0, 7)
+  }
+
+  function GetBackupVersion ($repoName) {
+    $backups = Get-ChildItem -Path (GetBackupPath $repoName "*")
+    if (!$backups) { return $null }
+    return $backups[0].BaseName.Split(".")[2]
+  }
+
+  function GetTempPath ($repoName) {
+    return "$(Join-Path -Path $env:TEMP -ChildPath $repoName).git"
+  }
+
+  function GetBackupPath ($repoName, $version) {
+    return "$(Join-Path -Path $To -ChildPath $repoName).git.$version.zip"
+  }
+
+  function Clone ($url, $path) {
+    git clone --bare --quiet $url $path
+  }
+
+  function Remove ($path) {
+    Remove-Item -Path $path -Recurse -Force -ErrorAction SilentlyContinue
+  }
+
+  function Zip ($from, $to) {
+    if (Get-Command "7z" -ErrorAction SilentlyContinue) {
+      7z a $to $from | Out-Null
+    } else {
+      Compress-Archive $from $to
+    }
+  }
+
+  function StartTimer {
+    return [Diagnostics.Stopwatch]::StartNew()
+  }
+
+  function StopTimer ($timer, $message) {
+    $timer.Stop()
+    Write-Host "$message in $($timer.Elapsed.ToString("mm\:ss\.fff"))" -ForegroundColor DarkGray
+  }
+
+  function Log ($repoName) {
+    Write-Host "Processing " -NoNewLine
+    Write-Host $repoName -ForegroundColor DarkCyan
+  }
+
+  $allTime = StartTimer
+
+  foreach ($repo in (GetRepos)) {
+    $time = StartTimer
+    Log $repo.name
+    $backupVersion = GetBackupVersion $repo.name
+    $repoVersion = GetRepoVersion $repo.name
+    if ($backupVersion -eq $repoVersion) {
+      Write-Host "Backup version $backupVersion is up to date" -ForegroundColor DarkGray
+      continue
+    }
+    Write-Host "Updating backup to version $repoVersion" -ForegroundColor DarkGray
+    $tempPath = GetTempPath $repo.name
+    Remove $tempPath
+    Remove (GetBackupPath $repo.name $backupVersion)
+    Clone $repo.url $tempPath
+    Zip $tempPath (GetBackupPath $repo.name $repoVersion)
+    Remove $tempPath
+    StopTimer $time "Done"
+  }
+
+  StopTimer $allTime "All done"
+}
+
+Set-Alias repos Backup-Repos
+
