a4a4f3a5adc2975d067a4e484c2eb5a09aa4d224
greg <grzegorz.kozub@gmail.com>
2019-12-12 10:30:59 +0100

    Merge scripts that reset explorer items

--- Reset-DialogPaths.ps1
+++
@@ -1,7 +0,0 @@
-function Reset-DialogPaths {
-    Remove-Item `
-        -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32" `
-        -Recurse `
-        -ErrorAction SilentlyContinue
-}
-
---
+++ Reset-Explorer.ps1
@@ -0,0 +1,64 @@
+function Reset-Explorer {
+  [CmdletBinding()]
+
+  param (
+    [Parameter(Position = 0)]
+    [switch]
+    $DialogPaths,
+
+    [Parameter(Position = 1)]
+    [switch]
+    $TrayIcons,
+
+    [Parameter(Position = 2)]
+    [switch]
+    $Windows
+  )
+
+  function Log ($message) {
+    Write-Host $message -ForegroundColor DarkGray
+  }
+
+  function DialogPaths {
+    Log "Clearing paths recently used in dialog windows"
+    Remove-Item `
+      -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32" `
+      -Recurse `
+      -ErrorAction SilentlyContinue
+  }
+
+  function TrayIcons {
+    Log "Clearing tray icons"
+    Stop-Process -Name "explorer"
+    foreach ($value in "IconStreams", "PastIconsStream") {
+      Remove-ItemProperty `
+        -Name $value `
+        -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify" `
+        -ErrorAction SilentlyContinue
+    }
+  }
+
+  function Windows {
+    Log "Clearing manually set window sizes"
+    $keys = (
+      "HKCU:\Software\Microsoft\Windows\Shell\BagMRU", 
+      "HKCU:\Software\Microsoft\Windows\Shell\Bags", 
+      "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\BagMRU", 
+      "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\Shell\Bags"
+    )
+    foreach ($key in $keys) {
+      Remove-Item -Path $key -Recurse -ErrorAction SilentlyContinue
+    }
+  }
+
+  if (!$DialogPaths -and !$Windows -and !$TrayIcons) {
+    $DialogPaths = $true
+    $TrayIcons = $true
+    $Windows = $true
+  }
+
+  if ($DialogPaths) { DialogPaths }
+  if ($TrayIcons) { TrayIcons }
+  if ($Windows) { Windows }
+}
+
