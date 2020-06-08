f849c855f29058d3097aa0e96dc61009fa14223b
greg <grzegorz.kozub@gmail.com>
2020-06-08 21:31:59 +0200

    Remove Deploy module and merge code to Deploy-App

--- Deploy-App.ps1
+++ Deploy-App.ps1
@@ -2,7 +2,6 @@
 using namespace System.Management.Automation
 
 Import-Module Admin
-Import-Module Deploy
 
 function Deploy-App {
   param (
@@ -99,29 +98,61 @@ function Deploy-App {
       $hook = "add"
     }
 
-    function RemoveUnpacked {
-      Remove $globals.unpacked
+    function Log ($action, $path) {
+      Write-Host ("{0,-7}" -f $action) -NoNewLine
+      Write-Host $path -ForegroundColor DarkCyan
+    }
+
+    function Remove ($path) {
+      if (!(Test-Path $path)) { return }
+      Log "Remove" $path
+      Remove-Item $path -Recurse -Force
+    }
+
+    function RemoveUnpacked { Remove $globals.unpacked }
+
+    if (Test-Path $globals.unpacked) { RemoveUnpacked }
+
+    function Test7z { return !!(Get-Command "7z" -ErrorAction SilentlyContinue) }
+
+    function Unzip ($from, $to) {
+      Log "Unzip" (Join-Path $to ([IO.Path]::GetFileNameWithoutExtension($from)))
+      if (Test7z) {
+        7z x $from -y -o"$to" | Out-Null
+      } else {
+        Expand-Archive $from $to
+      }
+    }
+
+    function SetOwner ($path, $user) {
+      $acl = Get-Acl -LiteralPath $path
+      $acl.SetOwner($user)
+      Set-Acl -LiteralPath $path -AclObject $acl
+    }
+
+    function GetCurrentUser {
+      return New-Object System.Security.Principal.NTAccount($env:USERNAME)
+    }
+
+    function SetOwnerToCurrentUser ($path) {
+      SetOwner $path (GetCurrentUser)
     }
 
     function UnzipPackage {
       Unzip $globals.package $globals.unpackedDir
       SetOwnerToCurrentUser $globals.unpacked
+
+      function SetChildrenOwnerToCurrentUser ($path) {
+        $user = GetCurrentUser
+        foreach ($item in (Get-ChildItem $path -Recurse)) {
+          SetOwner $item.FullName $user
+        }
+      }
+
       SetChildrenOwnerToCurrentUser $globals.unpacked
     }
 
-    if (Test-Path $globals.unpacked) { RemoveUnpacked }
     UnzipPackage
-
-    function DeployCategory ($category, $to, $replace = $true, $createLinks = $true) {
-      $from = Join-Path $globals.unpacked $category
-
-      $deviceFrom = $from + "@" + $env:COMPUTERNAME
-      if (Test-Path $deviceFrom) {
-        $from = $deviceFrom
-      } elseif (!(Test-Path $from)) { return }
-
-      DeployItems $switches $globals $from $to $replace $createLinks
-    }
 
     function RunPs1 ($name) {
       if ($switches.skipPs1) { return }
@@ -133,6 +164,75 @@ function Deploy-App {
     }
 
     if (!$switches.remove -and !$switches.pack) { RunPs1 "install" }
+
+    function DeployCategory ($category, $to, $replace = $true, $createLinks = $true) {
+      $from = Join-Path $globals.unpacked $category
+
+      $deviceFrom = $from + "@" + $env:COMPUTERNAME
+      if (Test-Path $deviceFrom) { $from = $deviceFrom } elseif (!(Test-Path $from)) { return }
+
+      $isC = $to.StartsWith($globals.systemDrive)
+
+      function CreateDir ($dir) {
+        if (Test-Path $dir) { return }
+        Log "Create" $dir
+        New-Item $dir -ItemType Directory | Out-Null
+      }
+
+      function CreateCopy ($from, $to, $isDir) {
+        Log "Copy" $to
+        if ($isDir) {
+          robocopy $from $to /NJH /NJS /NFL /NDL /E /COPY:DATO "/MT:$($env:NUMBER_OF_PROCESSORS / 2)" | Out-Null
+        } else {
+          robocopy `
+            ([IO.Path]::GetDirectoryName($from)) `
+            ([IO.Path]::GetDirectoryName($to)) `
+            ([IO.Path]::GetFileName($from)) `
+            /NJH /NJS /NFL /NDL /COPY:DATO | Out-Null
+        }
+      }
+
+      function CreateLink ($link, $path, $isDir = $true) {
+        if (Test-Path $link) { return }
+        Log "Link" $link
+        if ($isDir) {
+          New-Item -ItemType Junction -Path $link -Target $path | Out-Null
+        } else {
+          New-Item -ItemType SymbolicLink -Path $link -Target $path | Out-Null
+        }
+        SetOwnerToCurrentUser $link
+      }
+
+      foreach ($itemFrom in Get-ChildItem $from -Force -ErrorAction SilentlyContinue) {
+        $itemTo = Join-Path $to $itemFrom.Name
+        $isDir = $itemFrom.Attributes.HasFlag([IO.FileAttributes]::Directory)
+
+        if (($isC -and !$switches.skipC) -or (!$isC -and !$switches.skipD)) {
+          if (!$switches.pack -and ($switches.remove -or $replace)) {
+            Remove $itemTo
+          }
+
+          if ($switches.pack) {
+            Remove $itemFrom.FullName
+            CreateCopy $itemTo $itemFrom.FullName $isDir
+          }
+
+          if (!$switches.remove -and !$switches.pack) {
+            CreateDir $to
+            CreateCopy $itemFrom.FullName $itemTo $isDir
+          }
+        }
+
+        if (!$switches.skipC -and !$switches.pack -and $createLinks) {
+          $link = Join-Path $globals.systemDrive $itemTo.TrimStart($globals.targetDrive)
+          Remove $link
+          if (!$switches.remove) {
+            CreateDir (Join-Path $globals.systemDrive $to.TrimStart($globals.targetDrive))
+            CreateLink $link $itemTo $isDir
+          }
+        }
+      }
+    }
 
     DeployCategory "apps" $d.apps $true $false
     DeployCategory "programdata" $d.programdata
@@ -180,11 +280,22 @@ function Deploy-App {
       $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
     }
 
+    function RefreshIcons { ie4uinit -show }
     if (!$switches.remove -and !$switches.pack) { RefreshIcons }
 
     if ($switches.pack) {
       $zip = "$($globals.unpacked).zip"
       Remove $zip
+
+      function Zip ($from, $to) {
+        Log "Zip" $to
+        if (Test7z) {
+          7z a $to $from | Out-Null
+        } else {
+          Compress-Archive $from $to
+        }
+      }
+
       Zip $globals.unpacked $zip
       SetOwnerToCurrentUser $zip
       Log "Move" $globals.package
