2dfe88514c32a4306ee8e2d897ca5f5da052527b
greg <grzegorz.kozub@gmail.com>
2021-05-24 22:04:42 +0200

    Add palette.ps1

---
+++ palette.ps1
@@ -0,0 +1,7 @@
+﻿foreach ($color in [enum]::GetValues([System.ConsoleColor])) {
+  Write-Host "  " -BackgroundColor $color -NoNewline
+  Write-Host $([int]$color).ToString().PadLeft(2, "0") -ForegroundColor $color -NoNewline
+  Write-Host " " -NoNewline
+}
+Write-Host ""
+
