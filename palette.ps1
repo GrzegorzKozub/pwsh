ffe0f159107ecc13bf2b1c35ebb79904fcf6cc83
greg <grzegorz.kozub@gmail.com>
2021-05-24 22:32:46 +0200

    Match palette.ps1 with terminal color indicies

--- palette.ps1
+++ palette.ps1
@@ -1,8 +1,28 @@
-﻿foreach ($color in [enum]::GetValues([System.ConsoleColor])) {
-  Write-Host ■ -ForegroundColor $color -NoNewline
+﻿$colors = @(
+  [System.ConsoleColor]::Black,
+  [System.ConsoleColor]::DarkRed,
+  [System.ConsoleColor]::DarkGreen,
+  [System.ConsoleColor]::DarkYellow,
+  [System.ConsoleColor]::DarkBlue,
+  [System.ConsoleColor]::DarkMagenta,
+  [System.ConsoleColor]::DarkCyan,
+  [System.ConsoleColor]::Gray,
+ 
+  [System.ConsoleColor]::DarkGray,
+  [System.ConsoleColor]::Red,
+  [System.ConsoleColor]::Green,
+  [System.ConsoleColor]::Yellow,
+  [System.ConsoleColor]::Blue,
+  [System.ConsoleColor]::Magenta,
+  [System.ConsoleColor]::Cyan,
+  [System.ConsoleColor]::White
+)
+
+for ($i=0; $i -lt $colors.Length; $i++) {
+  Write-Host ■ -ForegroundColor $colors[$i] -NoNewline
   Write-Host " " -NoNewline
-  Write-Host $([int]$color).ToString().PadLeft(2, "0") -ForegroundColor $color -NoNewline
+  Write-Host $i.ToString().PadLeft(2, "0") -ForegroundColor $colors[$i] -NoNewline
   Write-Host " " -NoNewline
-  Write-Host $color -ForegroundColor $color
+  Write-Host $colors[$i] -ForegroundColor $colors[$i]
 }
 
