f02928442e2d97025b91c7283508e96a7b3762ed
greg <grzegorz.kozub@gmail.com>
2022-03-13 20:10:52 +0100

    Cpu max still doesnt work in qemu

--- vm.ps1
+++ vm.ps1
@@ -1,14 +1,14 @@
 qemu-system-x86_64 `
--name windows `
--smp 4,sockets=1,cores=2,threads=2 `
--m 8G `
--vga none -device qxl-vga,vgamem_mb=64,ram_size_mb=128,vram_size_mb=256,vram64_size_mb=256 `
--accel whpx,kernel-irqchip=off `
--device ich9-intel-hda `
--device hda-output `
--nic user,model=virtio-net-pci,smb=c:\users\greg\downloads `
--usbdevice tablet `
--drive file=windows.cow,if=virtio,aio=native,cache.direct=on `
--boot menu=on `
--monitor stdio
+  -name windows `
+  -smp 4,sockets=1,cores=2,threads=2 `
+  -m 8G `
+  -accel whpx,kernel-irqchip=off `
+  -vga none -device qxl-vga,vgamem_mb=64,ram_size_mb=128,vram_size_mb=256,vram64_size_mb=256 `
+  -nic user,model=virtio-net-pci,smb=d:\users\greg\downloads `
+  -usbdevice tablet `
+  -drive file=d:\software\windows.cow,if=virtio,aio=native,cache.direct=on `
+  -boot menu=on `
+  -monitor stdio
+
+  # -cpu max,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,topoext `
 
