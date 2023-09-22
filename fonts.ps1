2852ac391347196701487a68eceed5a953165276
greg <grzegorz.kozub@gmail.com>
2023-09-22 20:31:58 +0200

    font test script

---
+++ fonts.ps1
@@ -0,0 +1,7 @@
+$e=[char]27 # esc
+
+Write-Host "styles:     normal, $e[1mbold$e[0m, $e[3mitalic$e[0m, $e[3;1mbold italic$e[0m, $e[4munderline$e[0m"
+Write-Host "ligatures:  == != === !== >= <= => ->"
+Write-Host "nerd fonts:               "
+Write-Host "emoji:      🙁 😐 🙂 👍 👎"
+
