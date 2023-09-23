ee7f5d17a08987acafc5b8fe90e06228ff641ee4
greg <grzegorz.kozub@gmail.com>
2023-09-23 09:07:31 +0200

    refactor

--- fonts.ps1
+++ fonts.ps1
@@ -1,7 +1,13 @@
-$e=[char]27 # esc
+Write-Host "styles:     " -NoNewline
+Write-Host "normal, " -NoNewline
+Write-Host "$([char]27)[1mbold$([char]27)[0m, " -NoNewline
+Write-Host "$([char]27)[3mitalic$([char]27)[0m, " -NoNewline
+Write-Host "$([char]27)[3;1mbold italic$([char]27)[0m, " -NoNewline
+Write-Host "$([char]27)[4munderline$([char]27)[0m"
 
-Write-Host "styles:     normal, $e[1mbold$e[0m, $e[3mitalic$e[0m, $e[3;1mbold italic$e[0m, $e[4munderline$e[0m"
 Write-Host "ligatures:  == != === !== >= <= => ->"
+
 Write-Host "nerd fonts:               "
+
 Write-Host "emoji:      🙁 😐 🙂 👍 👎"
 
