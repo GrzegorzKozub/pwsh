e0d454cc4bdd10c6e6d2b5e442830a4e27692586
greg <grzegorz.kozub@gmail.com>
2024-02-26 08:05:54 +0100

    rem btop

--- bt.ps1
+++
@@ -1,9 +0,0 @@
-if ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains "S-1-5-32-544") {
-  &btop
-} else {
-  Start-Process `
-    -Verb "RunAs" `
-    -FilePath "wt" `
-    -ArgumentList "new-tab", "pwsh", "-Command", "btop"
-}
-
