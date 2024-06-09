a76c131e9d63114222a4882259bae4b667e2e435
greg <grzegorz.kozub@gmail.com>
2024-06-09 06:26:31 +0200

    set term and colorterm in wt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -38,9 +38,12 @@ Set-PSReadLineKeyHandler -ViMode Command -Chord "v,v" -Function ViEditVisually #
 $env:LS_COLORS = "rs=0:di=36:ln=34:pi=33:so=35:do=35:bd=33:cd=33:or=31:mi=0:tw=36:ow=36:st=36:ex=32:*.7z=33:*.gz=33:*.rar=33:*.tar=33:*.zip=33:*.cow=35:*.fsa=35:*.mrimg=35:*.iso=35:*.wim=35:*.jpeg=35:*.jpg=35:*.png=35:*.svg=35:*.mp3=35:*.ogg=35:*.opus=35:*.mkv=35:*.mp4=35:*.webm=35:*.dockerignore=37:*.editorconfig=37:*.eslintrc=37:*.git=37:*.gitattributes=37:*.gitignore=37:*.gitmodules=37:*.pylintrc=37:*.backup=90:*.bak=90:*.log=90:*.off=90:*.old=90:*.orig=90:*.original=90:*.part=90:*.swp=90:*.tmp=90"
 $env:LS_COLORS = $env:LS_COLORS + ":*.bat=32:*.cmd=32:*.exe=32:*.ps1=32"
 
-# bat
+# terminal features
 
-$env:COLORTERM = "truecolor" # fix bat color tint
+if ($env:WT_SESSION) { # https://github.com/microsoft/terminal/issues/11057
+  $env:COLORTERM = "truecolor" # fix bat color tint
+  # $env:TERM = "xterm-256color" # clear screen when neovim exits
+}
 
 # eza
 
@@ -126,7 +129,6 @@ Set-PSReadLineKeyHandler -Chord "escape,l" -ViMode Command -ScriptBlock {
 # neovim
 
 $env:EDITOR = $env:VISUAL = "nvim"
-# $env:TERM = "xterm-256color" # clear screen when neovim exits
 
 Set-Alias -Name vim -Value nvim
 
