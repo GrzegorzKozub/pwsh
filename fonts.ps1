48e83aa1f2e8b55ccfe09b0947a2c684885f9c95
greg <grzegorz.kozub@gmail.com>
2025-01-02 20:02:11 +0100

    test terminal link

--- fonts.ps1
+++ fonts.ps1
@@ -1,16 +1,24 @@
-Write-Host "styles:     " -NoNewline
-Write-Host "normal, " -NoNewline
-Write-Host "$([char]27)[1mbold$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[3mitalic$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[3;1mbold italic$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[4munderline$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[9mstrikethrough$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[5mblink$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[2mdim$([char]27)[0m, " -NoNewline
-Write-Host "$([char]27)[7mreverse$([char]27)[0m"
+Write-Host "normal " -NoNewline
 
-Write-Host "ligatures:  == != === !== >= <= => ->"
+Write-Host "$([char]27)[1mbold$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[2mdim$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[3mitalic$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[3;1mbold-italic$([char]27)[0m"
 
-Write-Host "nerd fonts:               "
+Write-Host "$([char]27)[4:1mstraight$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[4:2mdouble$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[4:3mcurly$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[4:4mdotted$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[4:5mdashed$([char]27)[0m"
 
-Write-Host "emoji:      🙁 😐 🙂 👍 👎"
+Write-Host "$([char]27)[5mblink$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[7mreverse$([char]27)[0m " -NoNewline
+Write-Host "$([char]27)[9mstrikethrough$([char]27)[0m"
+
+Write-Host "$([char]27)]8;;http://archlinux.org$([char]27)\link$([char]27)]8;;$([char]27)\"
+
+Write-Host "== != === !== >= <= => ->"
+Write-Host "              "
+Write-Host "🙁 😐 🙂 👍 👎"
+
+Write-Host
