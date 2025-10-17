75db6ec7641075bcdc749edffff1bd5d03a90df6
greg <grzegorz.kozub@gmail.com>
2025-10-17 21:53:57 +0200

    backup everything from d

--- backup.ps1
+++ backup.ps1
@@ -5,10 +5,18 @@ if (!$backup) {
 }
 
 $target = Join-Path -Path "${backup}:" -ChildPath "Windows"
-$dirs = "D:\Images", "D:\Music", "D:\Reflect", "D:\Software", "D:\Win"
+$dirs =
+  "D:\Apps",
+  "D:\Code",
+  "D:\Images",
+  "D:\Music",
+  "D:\Reflect",
+  "D:\Software",
+  "D:\Users",
+  "D:\Win"
 
 foreach ($dir in $dirs) {
   $copy = Join-Path -Path $target -ChildPath (Split-Path -Path $dir -Leaf)
   Write-Host "Backup `e[36m$dir`e[0m to `e[36m$copy`e[0m"
-  rclone sync --progress $dir $copy
+  rclone sync --copy-links --progress $dir $copy
 }
