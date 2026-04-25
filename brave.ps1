53c7127595e07e0d33f69b5bf1cda7c50a8831b0
greg <grzegorz.kozub@gmail.com>
2026-04-25 10:45:49 +0200

    support missing brave config

--- brave.ps1
+++ brave.ps1
@@ -6,11 +6,13 @@ param (
 if (Get-Process -Name brave -ErrorAction SilentlyContinue) { throw "Brave is running" }
 
 $config = Join-Path -Path $env:LOCALAPPDATA -ChildPath "BraveSoftware"
+$config = Join-Path -Path "D:" -ChildPath $config.TrimStart("C:")
 
 foreach ($brave in "Brave-Browser", "Brave-Origin-Nightly") {
-  $profiles = Join-Path -Path $config -ChildPath $brave
-  $currentProfile = Join-Path -Path $profiles -ChildPath "User Data"
-  $backupProfile = Join-Path -Path $profiles -ChildPath "User Data.backup"
+  $profile = Join-Path -Path $config -ChildPath $brave
+  if (!(Test-Path -Path $profile)) { continue }
+  $currentProfile = Join-Path -Path $profile -ChildPath "User Data"
+  $backupProfile = Join-Path -Path $profile -ChildPath "User Data.backup"
   if ($Backup) { rclone sync $currentProfile $backupProfile }
   if ($Restore) { rclone sync $backupProfile $currentProfile }
 }
