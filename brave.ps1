5bd18a29effa25051316bdc88239b651e7ed8eb0
greg <grzegorz.kozub@gmail.com>
2026-04-25 08:55:33 +0200

    brave origin backup

--- brave.ps1
+++ brave.ps1
@@ -5,16 +5,19 @@ param (
 
 if (Get-Process -Name brave -ErrorAction SilentlyContinue) { throw "Brave is running" }
 
-$zip = "D:\Software\Brave.zip"
-$profiles = Join-Path -Path $env:LOCALAPPDATA -ChildPath "BraveSoftware\Brave-Browser"
-$currentProfile = Join-Path -Path $profiles -ChildPath "User Data"
-$backupProfile = Join-Path -Path $profiles -ChildPath "User Data.backup"
+$config = Join-Path -Path $env:LOCALAPPDATA -ChildPath "BraveSoftware"
 
-if ($Backup) {
-  rclone sync $currentProfile $backupProfile
-  Remove-Item -Path $zip -ErrorAction SilentlyContinue -Force
-  7z a $zip $currentProfile $backupProfile | Out-Null
+foreach ($brave in "Brave-Browser", "Brave-Origin-Nightly") {
+  $profiles = Join-Path -Path $config -ChildPath $brave
+  $currentProfile = Join-Path -Path $profiles -ChildPath "User Data"
+  $backupProfile = Join-Path -Path $profiles -ChildPath "User Data.backup"
+  if ($Backup) { rclone sync $currentProfile $backupProfile }
+  if ($Restore) { rclone sync $backupProfile $currentProfile }
 }
 
-if ($Restore) { rclone sync $backupProfile $currentProfile }
+if ($Backup) {
+  $zip = "D:\Software\Brave.zip"
+  Remove-Item -Path $zip -ErrorAction SilentlyContinue -Force
+  7z a $zip $config | Out-Null
+}
 
